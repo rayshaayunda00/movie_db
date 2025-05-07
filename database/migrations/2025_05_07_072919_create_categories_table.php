1f229f297c7bfa933347a9b0d7eb614544c7fe76
ASUS <rayshaayuda@gmail.com>
2025-05-07 14:46:18 +0700

    first commit

---
+++ database/migrations/2025_05_07_072919_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('categories', function (Blueprint $table) {
+        $table->id(); // bigint(20) unsigned auto_increment
+        $table->string('category_name', 128);
+        $table->string('description', 255);
+        $table->timestamp('deleted_at')->nullable();
+        $table->timestamps(); // created_at dan updated_at
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
